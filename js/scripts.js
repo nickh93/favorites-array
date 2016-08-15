695a8fb3ccdbb921518f9659585da62bfe502d43
Lisa MacCarrigan and Nick Hurtado <lisa@>
2016-08-15 10:15:20 -0400

    use JS to build new arrays out of specific elements from first array

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,16 @@ $(document).ready(function() {
     var season= $("#season").val();
     var animal= $("#animal").val();
     var destination= $("#destination").val();
-    var favorites = [color, food, season, animal, destination];
 
-    alert (favorites);
+    var favorites1 = [color, food, season, animal, destination];
+
+    var favorites2 = favorites1.slice(0,2); //color, food
+    var favorites3 = favorites2.reverse(); //food, color
+    var favorites4 = favorites3.concat(favorites1[2]); //food, color, season
+
+
+
+    alert (favorites4);
+
   });
 });
